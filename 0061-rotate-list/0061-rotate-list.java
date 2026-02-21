f8b6c557a48de63b195e895ce6cf3042028d0cb4
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-02-21 13:05:02 +0530

    Time: 0 ms (100%), Space: 44.1 MB (91.65%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,27 +10,41 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        ListNode temp = head ;
-        int n=1 ;
         if (head == null || head.next == null || k == 0)
             return head;
-        while(temp.next!=null){
-            temp = temp.next ;
-            n++;
+        int l = 0 ;
+        ListNode temp = head ;
+        while(temp!=null){
+            l++;
+            temp=temp.next ;
         }
-        temp.next = head ;
-        k = k%n ;
-        if(k==0) {
-            temp.next = null;
-            return head ;}
-        ListNode newHead = head ;
-        for(int i =0 ; i< n-k-1 ; i++){
-            newHead=newHead.next ;
+         k = k%l ;
+        ListNode temp2 = head ;
+        for(int i = 0 ;i<l-k-1 ; i++){
+            temp2 = temp2.next ;
         }
-        ListNode ans = newHead.next;
-        newHead.next = null ;
-        return ans;
+        ListNode n = temp2.next ;
+        temp2.next = null;
+        ListNode head2 = reverse(n);
+        while(head2!=null){
+            ListNode n2 = head2.next;
+            head2.next = head ;
+            head = head2 ;
+            head2 = n2;
+            
+        }
+        return head ;
 
-
+    }
+    static ListNode reverse(ListNode head){
+        ListNode prev = null ;
+        ListNode curr = head ;
+        while(curr!=null){
+            ListNode next = curr.next ;
+            curr.next = prev ;
+            prev = curr ;
+            curr = next ;
+        }
+        return prev ;
     }
 }

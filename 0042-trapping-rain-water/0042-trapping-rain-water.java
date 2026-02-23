33c05d54b5b7720add3d4f236031b7bb8bdd8193
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-02-23 15:31:31 +0530

    Time: 1 ms (60.31%), Space: 47.9 MB (57.7%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,78 +1,22 @@
-// class Solution {
-//     public int trap(int[] height) {
-//         if (height == null || height.length == 0) {
-//             return 0;
-//         }
-//         int ans = 0;
-//       for(int i =0;i<height.length;i++){
-//         int a = max(height,0,i);
-//         int b = max(height,i,height.length);
-//         int min = Math.min(a,b);
-//         if(min>height[i]){
-//             ans = ans + (min-height[i]);
-//         }
-//       }
-//       return ans;  
-//     }
-//     static int max(int[] arr , int start , int end){
-//         int max = arr[start];
-//         for(int i = start+1 ; i<end ; i++){
-//             if(max<arr[i]){
-//                 max = arr[i];
-//             }
-//         }
-//         return max;
-//     }
-// }
-// class Solution {
-//     public int trap(int[] height) {
-//         if (height == null || height.length == 0) {
-//             return 0;
-//         }
-//         int ans = 0;
-//         int n = height.length;
-
-//         for (int i = 0; i < n; i++) {
-//             int leftMax  = findMax(height, 0, i);
-//             int rightMax = findMax(height, i, n - 1);
-
-//             int minOfTwo = Math.min(leftMax, rightMax);
-//             if (minOfTwo > height[i]) {
-//                 ans += (minOfTwo - height[i]);
-//             }
-//         }
-
-//         return ans;
-//     }
-
-//     private static int findMax(int[] arr, int start, int end) {
-//         int m = arr[start];
-//         for (int i = start + 1; i <= end; i++) {
-//             if (arr[i] > m) {
-//                 m = arr[i];
-//             }
-//         }
-//         return m;
-//     }
-// }
-// Approach 3: Two-Pointer Approach (O(N) Time, O(1) Space)
 class Solution {
-    public int trap(int[] arr) {
-        int l = 0, r = arr.length - 1;
-        int lmax = 0, rmax = 0, ans = 0;
-
-        while (l < r) {
-            lmax = Math.max(lmax, arr[l]);
-            rmax = Math.max(rmax, arr[r]);
-
-            if (lmax < rmax) {
-                ans += lmax - arr[l];
-                l++;
-            } else {
-                ans += rmax - arr[r];
-                r--;
-            }
+    public int trap(int[] height) {
+        int ans = 0 ;
+        int i = 0 ;
+        int j = height.length-1 ;
+        int lmax = Integer.MIN_VALUE;
+        int rmax = Integer.MIN_VALUE;
+        while(i<j){
+             lmax = Math.max(lmax , height[i]);
+             rmax = Math.max(rmax , height[j]);
+             if(lmax<rmax){
+                ans+=lmax-height[i];
+                i++;
+             }
+             else{
+                ans+=rmax-height[j];
+                j--;
+             }
         }
-        return ans;
+        return ans ;
     }
 }

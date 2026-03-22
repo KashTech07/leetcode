aaba7c68d49c7747656b489c8b36b61e362b90be
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-03-22 12:26:17 +0530

    Time: 11 ms (70.2%), Space: 50.3 MB (50.47%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,12 +1,12 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int start = max(weights);
-        int end = sum(weights);
-        int ans = 0 ;
+        int start = max(weights) ;
+        int end = sum(weights) ;
+        int ans = 0;
         while(start<=end){
-            int mid = start + (end-start)/2 ;
-            if(CanBeShipped(weights , mid , days)){
-                ans= mid ;
+            int mid = start +(end-start)/2 ;
+            if(canTake(weights , days , mid)){
+                ans = mid ;
                 end = mid-1 ;
             }
             else{
@@ -15,33 +15,36 @@ class Solution {
         }
         return ans ;
     }
-    static boolean CanBeShipped(int[] arr , int mid , int k){
-        int days = 1 ;
+    static boolean canTake(int[] arr, int k , int mid){
         int sum = 0 ;
-        for(int i = 0 ; i<arr.length ; i++){
+        int day = 1 ;
+        for(int i= 0 ; i<arr.length ; i++ ) {
             if(sum+arr[i]>mid){
-                days++;
-                sum = arr[i];
+                day++ ;
+                sum = arr[i] ;
             }
-            else sum +=arr[i];}
-            
-        if(days<=k) return true ;
-        return false;
+            else{
+                sum+=arr[i] ;
+            }
+        }
+        if(day<=k) return true ;
+        return false ;
     }
     static int max(int[] arr){
         int ans = arr[0];
-        for(int i = 1 ; i<arr.length ; i++){
-            if(arr[i]>ans){
-               ans = arr[i] ;
-            }
-        }
-        return ans ;
+        for(int n : arr){            
+               if(n>ans){
+                ans= n ;
+            }}
+            return ans ;
     }
     static int sum(int[] arr){
-        int sum = 0 ;
-        for(int n : arr){
-            sum+=n;
-        }
-        return sum;
+        int ans = 0;
+        for(int n : arr){            
+               
+                ans += n ;
+            }
+            return ans ;
     }
+
 }

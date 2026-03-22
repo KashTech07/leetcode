65fbdc0b9dd11a30af8de4de606ed1101b865af2
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-03-22 11:38:32 +0530

    Time: 26 ms (74.92%), Space: 55.7 MB (39.3%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,18 +1,29 @@
+class pair{
+    int f ;
+    int s ;
+    pair(int f , int s){
+        this.f = f ;
+        this.s = s ;
+    }
+}
+
+
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> q = new PriorityQueue<>((a,b)->(b[0]*b[0]+b[1]*b[1])-(a[0]*a[0]+a[1]*a[1]));
-        int[][] ans = new int[k][2];
-        for(int i = 0 ; i<points.length ; i++){
-            q.add(points[i]);
-            if(q.size()>k){
-                q.poll();
-            }
-    }
-    int x = 0; 
-    while(!q.isEmpty()){
-        ans[x]=q.poll();
+       int[][] ans = new int[k][2] ;
+       PriorityQueue<pair> q = new PriorityQueue<>((a,b)->((a.f*a.f+ a.s * a.s)-(b.f*b.f + b.s*b.s))) ; 
+       for(int i = 0 ; i<points.length ; i++) {
+        q.add(new pair(points[i][0] , points[i][1])) ;
+       }
+       int j = 0 ;
+       int x = 0 ;
+       while(!q.isEmpty() && j<k){
+        pair p = q.poll() ;
+        ans[x][0] = p.f ;
+        ans[x][1] = p.s ;
         x++ ;
-    }
-    return ans ;
+        j++ ;
+       }
+       return ans ;
     }
 }

9a99cf43fafd771692fdf4a845379a629d3a9fe5
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-06-04 10:58:11 +0530

    Time: 16 ms (92.46%), Space: 45.5 MB (64.55%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,27 +1,68 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-       int mindiff =Integer.MAX_VALUE ;
-       Arrays.sort(nums);
-        int result = 0 ;
-       for(int i = 0 ; i<nums.length-1 ; i++){
-        if(i>0 && nums[i]==nums[i-1]) continue ;
-        int j = i+1 ;
-        int k = nums.length-1 ;
-         while(j<k){
+//        int mindiff =Integer.MAX_VALUE ;
+//        Arrays.sort(nums);
+//         int result = 0 ;
+//        for(int i = 0 ; i<nums.length-1 ; i++){
+//         if(i>0 && nums[i]==nums[i-1]) continue ;
+//         int j = i+1 ;
+//         int k = nums.length-1 ;
+//          while(j<k){
+//         int sum = nums[i]+nums[j]+nums[k] ;
+//         int diff = Math.abs(sum-target );
+//        if(mindiff>diff){
+//         mindiff = diff ;
+//         result = sum ;
+//        }
+//         if (sum == target) return target;
+//         if(sum>target){
+//             k-- ;
+//         }
+//         else{
+//             j++ ;
+//         }
+//        }}
+//        return result ;
+//     }
+// }
+int ans = 0 ;
+int MinDiff = Integer.MAX_VALUE ;
+Arrays.sort(nums) ;
+for(int i = 0 ; i<nums.length ; i++){
+    int j = i+1 ;
+    int k = nums.length-1 ;
+    while(j<k){
         int sum = nums[i]+nums[j]+nums[k] ;
-        int diff = Math.abs(sum-target );
-       if(mindiff>diff){
-        mindiff = diff ;
-        result = sum ;
-       }
+        int diff = Math.abs(target-sum) ;
+        if(diff<MinDiff){
+           MinDiff = diff ;
+           ans = sum ;
+        }
         if (sum == target) return target;
         if(sum>target){
-            k-- ;
+            k--;
         }
         else{
-            j++ ;
+            j++;
         }
-       }}
-       return result ;
     }
+    
 }
+return ans ;}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

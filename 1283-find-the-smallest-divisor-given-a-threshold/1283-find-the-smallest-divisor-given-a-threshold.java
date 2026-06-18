505e033cf3301e557660305c4c2c160ca2063ce0
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-06-18 11:39:20 +0530

    Time: 11 ms (55.78%), Space: 51 MB (52.02%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -1,22 +1,59 @@
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
-        int start = 1 ;
-        int end = max(nums) ;
-        int ans = 0 ;
-        while(start<=end){
-            int mid = start + (end-start)/2 ;
-            if(canDone(nums , threshold , mid)){
-                ans = mid ;
-                 end= mid-1 ;
+//         int start = 1 ;
+//         int end = max(nums) ;
+//         int ans = 0 ;
+//         while(start<=end){
+//             int mid = start + (end-start)/2 ;
+//             if(canDone(nums , threshold , mid)){
+//                 ans = mid ;
+//                  end= mid-1 ;
                 
-            }
-            else{
-               start = mid+1 ;
-            }
+//             }
+//             else{
+//                start = mid+1 ;
+//             }
+//         }
+//         return ans ;
+//     }
+//     static boolean canDone(int[] nums, int threshold , int mid) {
+//         int sum = 0 ;
+//         for(int i = 0 ; i<nums.length ; i++){
+//             if(nums[i]%mid!=0){
+//                 sum+=(nums[i]/mid)+1 ;
+//             }
+//             else{
+//                 sum+=nums[i]/mid ;
+//             }
+//         }
+//         if(sum<=threshold) return true ;
+//         return false ;
+//     }
+//     static int max(int[] arr){
+//         int ans = arr[0] ;
+//         for(int n : arr){
+//             if(ans<n){
+//                 ans = n ;
+//             }
+//         }
+//         return ans ;
+//     }
+// }
+    int start = 1 ; 
+    int end = max(nums) ;
+    int ans = 0;
+    while(start<=end){
+        int mid = start + (end-start)/2 ;
+        if(canBeDone(nums , mid , threshold)){
+            ans = mid  ;
+            end = mid-1 ;
         }
-        return ans ;
+        else{
+            start = mid+1 ;
+        }
     }
-    static boolean canDone(int[] nums, int threshold , int mid) {
+    return ans ;}
+    static boolean canBeDone(int[] nums , int mid , int t){
         int sum = 0 ;
         for(int i = 0 ; i<nums.length ; i++){
             if(nums[i]%mid!=0){
@@ -26,8 +63,7 @@ class Solution {
                 sum+=nums[i]/mid ;
             }
         }
-        if(sum<=threshold) return true ;
-        return false ;
+        return t>=sum ;
     }
     static int max(int[] arr){
         int ans = arr[0] ;
@@ -37,5 +73,4 @@ class Solution {
             }
         }
         return ans ;
-    }
-}
+    }}

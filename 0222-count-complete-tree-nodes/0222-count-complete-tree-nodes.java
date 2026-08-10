f5b9398b045941c1bb1a546ea540b1af602246f4
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-08-10 21:33:35 +0530

    Time: 0 ms (100%), Space: 49.7 MB (13.16%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -16,6 +16,29 @@
 class Solution {
     public int countNodes(TreeNode root) {
         if(root==null) return 0 ;
+        int lh = lheight(root.left) ;
+        int rh = rheight(root.right) ;
+        if(lh==rh) {
+            return (int)(Math.pow(2,lh+1))-1;
+        }
         return 1+countNodes(root.left)+countNodes(root.right) ;
+    } 
+    static int lheight(TreeNode root){
+        int h = 0 ;
+        TreeNode temp = root ;
+        while(temp!=null){
+           temp=temp.left ;
+           h++ ;
+        }
+        return h ;
+    }
+    static int rheight(TreeNode root){
+        int h = 0 ;
+        TreeNode temp = root ;
+        while(temp!=null){
+           temp=temp.right ;
+           h++ ;
+        }
+        return h ;
     }
 }

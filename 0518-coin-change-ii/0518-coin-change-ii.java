139a696682fc2b4c4f60594bf94750cc01c8b9de
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-08-29 09:24:16 +0530

    Time: 26 ms (21.38%), Space: 58.3 MB (6.36%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,19 +1,34 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[amount+1][coins.length] ;
-        for(int[] r : dp)
-        Arrays.fill(r , -1) ;
-        return solve(coins , amount , 0 , dp) ;
-    }
-    static int solve(int[] arr , int amount , int idx , int[][] dp){
-        if(amount==0) return 1 ;
-        if(idx==arr.length) return 0 ;
-       // if(amount<arr[idx]) return 0 ;
-        if(dp[amount][idx]!=-1) return dp[amount][idx] ;
-        int take = 0;
-        if(arr[idx]<=amount)
-         take =  solve(arr , amount-arr[idx] , idx , dp) ;
-        int notake = solve(arr , amount ,idx+1 , dp) ;
-        return dp[amount][idx]=take+notake ;
-    }
+    //     int[][] dp = new int[amount+1][coins.length] ;
+    //     for(int[] r : dp)
+    //     Arrays.fill(r , -1) ;
+    //     return solve(coins , amount , 0 , dp) ;
+    // }
+    // static int solve(int[] arr , int amount , int idx , int[][] dp){
+    //     if(amount==0) return 1 ;
+    //     if(idx==arr.length) return 0 ;
+    //    // if(amount<arr[idx]) return 0 ;
+    //     if(dp[amount][idx]!=-1) return dp[amount][idx] ;
+    //     int take = 0;
+    //     if(arr[idx]<=amount)
+    //      take =  solve(arr , amount-arr[idx] , idx , dp) ;
+    //     int notake = solve(arr , amount ,idx+1 , dp) ;
+    //     return dp[amount][idx]=take+notake ;
+    // }
+int[][] dp = new int[amount+1][coins.length+1] ;
+for(int[] r : dp){
+    Arrays.fill(r , -1) ;
 }
+return solve(coins , 0 , amount , dp) ;}
+static int solve(int[] arr , int idx , int amount , int[][] dp){
+    if(amount==0) return 1 ; 
+    if(idx==arr.length) return  0 ;
+    if(dp[amount][idx]!=-1) return dp[amount][idx] ;
+    int taken = 0 ;
+    int nottaken = solve(arr , idx+1 , amount , dp) ;
+    if(amount>=arr[idx]){
+        taken = solve(arr , idx , amount-arr[idx] , dp) ;
+    }
+    return dp[amount][idx] = taken+nottaken ;
+}}

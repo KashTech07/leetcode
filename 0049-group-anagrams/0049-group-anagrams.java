2f69e204e505461607b293bd43861841edee61c8
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-09-02 12:15:29 +0530

    Time: 7 ms (72.77%), Space: 49.9 MB (25.9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,20 +1,33 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String , ArrayList<String>> map = new HashMap<>();
-        List<List<String>> ans = new ArrayList<>();
-        for(String s : strs){
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String a = new String(arr);
-            if(map.containsKey(a)){
-             map.get(a).add(s);
-            }else{
-                ArrayList<String> list = new ArrayList<>();
-                list.add(s);
-                map.put(a,list);
-            }
+        // HashMap<String , ArrayList<String>> map = new HashMap<>();
+        // List<List<String>> ans = new ArrayList<>();
+        // for(String s : strs){
+        //     char[] arr = s.toCharArray();
+        //     Arrays.sort(arr);
+        //     String a = new String(arr);
+        //     if(map.containsKey(a)){
+        //      map.get(a).add(s);
+        //     }else{
+        //         ArrayList<String> list = new ArrayList<>();
+        //         list.add(s);
+        //         map.put(a,list);
+        //     }
+        // }
+        // for(String key : map.keySet()){
+        //     ans.add(map.get(key));
+        // }
+        // return ans ; }}
+        HashMap<String , ArrayList<String>> map = new HashMap<>() ;
+        List<List<String>> ans = new ArrayList<>() ;
+        for(int i = 0 ; i<strs.length ; i++){
+            char[] arr = strs[i].toCharArray() ;
+            Arrays.sort(arr) ;
+            String s = new String(arr) ;
+            map.put(s , map.getOrDefault(s ,new ArrayList<String>())) ;
+            map.get(s).add(strs[i]) ;
         }
-        for(String key : map.keySet()){
-            ans.add(map.get(key));
+        for(String k : map.keySet()){
+          ans.add(map.get(k)) ;
         }
         return ans ; }}

b235e79d1ae47fe8e47d02413dd9328ea9aca6a9
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-09-07 22:14:39 +0530

    Time: 5 ms (94.73%), Space: 46.5 MB (63.73%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,33 +1,23 @@
-class pair{
-    char c; 
-    int freq = 0 ;
-    pair( int freq , char c ){
-        this.c = c ;
-        this.freq = freq ;
-    }
-}
-
 class Solution {
     public String frequencySort(String s) {
-       HashMap<Character , Integer> map = new HashMap<>();
-       String ans = "" ;
-       PriorityQueue<pair> q = new PriorityQueue<>((a,b)->b.freq-a.freq) ;
-       for(int i=0 ; i<s.length() ; i++){
-       map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) , 0)+1) ;
-       } 
-       for(char ch : map.keySet()){
-        q.add(new pair(map.get(ch) , ch));
-       }
-       while(!q.isEmpty()){
-        pair p = q.poll() ;
-        while(p.freq>0){
-        ans+=p.c ;
-         p.freq--;
-        // if(p.freq>0){
-        //     q.add(new pair(p.freq , p.c));
-        // }
-       }}
-       return ans ;
-
+        int[] freq = new int[128] ;
+        for(int i = 0 ; i<s.length() ; i++){
+            freq[s.charAt(i)]++ ;
+        }
+        StringBuilder sb = new StringBuilder() ;
+        int n = s.length() ;
+        while(sb.length()<n){
+            int max = 0 ;
+            for(int i = 1 ; i<128 ; i++){
+                if(freq[i]>freq[max]){
+                    max = i ;
+                }
+            }
+            for(int i = 0 ; i<freq[max] ; i++){
+                sb.append((char) max) ;
+            }
+            freq[max] = 0 ;
+        }
+        return sb.toString() ;
     }
 }

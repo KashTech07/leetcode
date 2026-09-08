c4e704e994380fbeba7f23f7a7cbe85caa702080
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-09-08 21:00:32 +0530

    Time: 0 ms (100%), Space: 42.6 MB (73.54%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,25 +1,40 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
        HashMap<Character , String > map = new HashMap<>();
-       HashMap<String , Character > map2 = new HashMap<>();
-       String[] arr = s.split(" +");
-       if(pattern.length()!=arr.length){
-        return false;
-       }
-       for(int i = 0 ;i<pattern.length() ; i++){
-        if(map.containsKey(pattern.charAt(i))){
-            if(!map.get(pattern.charAt(i)).equals(arr[i])){
-                return false ;
-            }
-        }
-        if(map2.containsKey(arr[i])){
-            if(map2.get(arr[i])!=pattern.charAt(i)){
-                return false ;
-            }
-        }
-        map.put(pattern.charAt(i),arr[i]);
-        map2.put(arr[i],pattern.charAt(i));
-       } 
-       return true ;
+//        HashMap<String , Character > map2 = new HashMap<>();
+//        String[] arr = s.split(" +");
+//        if(pattern.length()!=arr.length){
+//         return false;
+//        }
+//        for(int i = 0 ;i<pattern.length() ; i++){
+//         if(map.containsKey(pattern.charAt(i))){
+//             if(!map.get(pattern.charAt(i)).equals(arr[i])){
+//                 return false ;
+//             }
+//         }
+//         if(map2.containsKey(arr[i])){
+//             if(map2.get(arr[i])!=pattern.charAt(i)){
+//                 return false ;
+//             }
+//         }
+//         map.put(pattern.charAt(i),arr[i]);
+//         map2.put(arr[i],pattern.charAt(i));
+//        } 
+//        return true ;
+//     }
+// }
+
+String[] arr = s.split(" ") ;
+if(pattern.length()!=arr.length) return false ;
+for(int i = 0 ; i<arr.length ; i++){
+    char c = pattern.charAt(i) ;
+    String word = arr[i] ;
+    if(map.containsKey(c)){
+        if(!map.get(c).equals(word)) return false ;
     }
+    else{
+        if(map.containsValue(word)) return false ;
+    }
+    map.put(c , word) ;
 }
+return true ; }}

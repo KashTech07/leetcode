b413f5acc619e17feecb499abb5425c37b788ac1
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-09-08 20:40:44 +0530

    Time: 14 ms (50.86%), Space: 43.8 MB (58.56%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,26 +1,31 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character , Character> map1 = new HashMap<>();
-        HashMap<Character , Character> map2 = new HashMap<>();
-        if(s.length()!=t.length()){
-            return false;
-        }
-        for(int i = 0 ; i <s.length() ; i++){
-            char a = s.charAt(i);
-            char b = t.charAt(i);
-            if(map1.containsKey(a)){
-                if(map1.get(a)!=b){
-                    return false;
-                }}
-                if(map2.containsKey(b)){
-                    if(map2.get(b)!=a){
-                        return false;
-                    }
-                }
-                map1.put(a,b);
-                map2.put(b,a);
-            }
-            return true;
-        
+        HashMap<Character,Character> map = new HashMap<>() ;
+//          HashMap<Character,Character> map2 = new HashMap<>() ;
+//          for(int i = 0 ; i<s.length() ; i++){
+//             char a = s.charAt(i) ;
+//             char b = t.charAt(i) ;
+//             if(map1.containsKey(a)){
+//                 if(map1.get(a)!=b) return false ;
+//              }
+//              if(map2.containsKey(b)){
+//                 if(map2.get(b)!=a) return false ;
+//              }
+//              map1.put(a,b) ;
+//              map2.put(b,a) ;
+//          }
+//          return true ;
+//     }
+// }
+for(int i = 0 ; i<s.length() ; i++){
+    char a = s.charAt(i) ;
+    char b = t.charAt(i) ;
+    if(map.containsKey(a)){
+        if(map.get(a)!=b) return false ;
     }
+    else{
+        if(map.containsValue(b)) return false ;
+    }
+    map.put(a,b) ;
 }
+return true ; }}

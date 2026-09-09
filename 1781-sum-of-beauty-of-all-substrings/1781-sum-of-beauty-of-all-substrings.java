25c8f55e430f255b23cce368f1b4715ae4769713
Kashish Sharma <kashishsuneetasharma@gmail.com>
2026-09-09 10:08:23 +0530

    Time: 83 ms (37.24%), Space: 44.1 MB (72.71%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -1,23 +1,38 @@
 class Solution {
     public int beautySum(String s) {
-        int beauty = 0;
-        for(int j = 0 ; j<s.length() ; j++){
-            int[] freq = new int[26] ;
-            for(int i = j ; i<s.length() ; i++){
-            char c = s.charAt(i) ;
-            freq[c - 'a']++ ;
-            int maxfreq = Integer.MIN_VALUE ;
-            int minfreq = Integer.MAX_VALUE ;
-            for (int f : freq) {
-                    if (f > 0) { 
-                        maxfreq = Math.max(maxfreq, f);
-                        minfreq = Math.min(minfreq, f);
-                    }
-                }
-                beauty = beauty + (maxfreq - minfreq);
-            }
-        }
+//         int beauty = 0;
+//         for(int j = 0 ; j<s.length() ; j++){
+//             int[] freq = new int[26] ;
+//             for(int i = j ; i<s.length() ; i++){
+//             char c = s.charAt(i) ;
+//             freq[c - 'a']++ ;
+//             int maxfreq = Integer.MIN_VALUE ;
+//             int minfreq = Integer.MAX_VALUE ;
+//             for (int f : freq) {
+//                     if (f > 0) { 
+//                         maxfreq = Math.max(maxfreq, f);
+//                         minfreq = Math.min(minfreq, f);
+//                     }
+//                 }
+//                 beauty = beauty + (maxfreq - minfreq);
+//             }
+//         }
         
-        return beauty;
+//         return beauty;
+//     }
+// }
+ int beauty = 0 ;
+ for(int i = 0 ; i<s.length() ; i++){
+    int[] freq = new int[26] ;
+    for(int j = i ; j<s.length() ; j++){
+        freq[s.charAt(j)-'a']++ ;
+    
+    int maxx = Integer.MIN_VALUE ;
+    int min = Integer.MAX_VALUE ;
+    for(int k = 0 ; k<26 ; k++){
+        if(freq[k]>maxx) maxx = freq[k] ;
+        if(freq[k] > 0&&freq[k]<min) min = freq[k] ;
     }
-}
+    beauty = beauty + (maxx-min) ;
+ }}
+ return beauty ; }}
